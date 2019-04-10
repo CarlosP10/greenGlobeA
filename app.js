b741344dbac48de080dce03213df31d7c845c5c8
Carlos Paredes <00028516@uca.edu.sv>
2019-04-09 21:40:51 -0600

    agregando user

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/coin', coinRouter);
 
 
 //creando base de datos
-mongoose.connect('mongodb://carlos10:Carlos100@ds129393.mlab.com:29393/coin', {useNewUrlParser:true})
+mongoose.connect('mongodb://teamcoin:hola10@ds129393.mlab.com:29393/coin', {useNewUrlParser:true})
 .then(()=> {
   console.log("Connection to database succesful!");
 })
